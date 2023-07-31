d05bfffbc85718178cf8c2890aaef6a57aa19618
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-31 00:27:15 -0300

    adds auth routes and error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
+import { errorHandler } from './middlewares';
+import { authRoute } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/auth', authRoute);
+app.use(errorHandler);
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
