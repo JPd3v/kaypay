b2b329add88326a5a1ce1965cbf5d8e2d90212e1
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-31 00:06:17 -0300

    adds schemaValidation middleware...
    which is used to validate user inputs, receives a zod object or
    zod effect as parameter and returns the middleware

---
+++ src/middlewares/schemaValidation.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, ZodEffects } from 'zod';
+
+function schemaValidation(schema: AnyZodObject | ZodEffects<AnyZodObject>) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({body:req.body,params:req.params,query:req.query});
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+export default schemaValidation;
