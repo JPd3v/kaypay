a193cedd65aaa1184e7b99355b6484251b0fea76
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-08-16 06:34:15 -0300

    adds update user service and sql query

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,12 +1,25 @@
+const sqlSelectAlias = {
+  userAliases: `first_name AS "firstName", last_name AS "lastName",refresh_token AS refreshToken,balance,password, alias, email, id`,
+};
+
 const auth = {
   addUser: `INSERT INTO users (first_name, last_name, alias, email, password )
   VALUES($1,$2,$3,$4,$5)
-  Returning first_name AS "firstName", last_name AS "lastName", alias, email, id`,
+  Returning first_name AS "firstName", last_name AS "lastName", alias, email, id, balance`,
 };
 
 const users = {
-  findUserByEmail: `SELECT * FROM users WHERE email ILIKE $1`,
-  findUserByAlias: `SELECT * FROM users WHERE alias ILIKE $1`,
+  findUserByEmail: `SELECT ${sqlSelectAlias.userAliases} FROM users WHERE email ILIKE $1`,
+  findUserByAlias: `SELECT ${sqlSelectAlias.userAliases} FROM users WHERE alias ILIKE $1`,
+  updateUser: `UPDATE USERS 
+  SET first_name = COALESCE($1,first_name),
+  last_name = COALESCE($2,last_name),
+  alias = COALESCE($3,alias),
+  email = COALESCE($4,email),
+  password = COALESCE($5,password),
+  refresh_token = COALESCE($6,refresh_token),
+  balance = COALESCE($7,balance)
+  WHERE id = $8 RETURNING ${sqlSelectAlias.userAliases}`,
 };
 
 const testingUtils = {
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,12 +1,28 @@
+import { AtLeast, User } from '@src/types';
 import { pool, queries } from '../database';
 
 async function getUserByEmail(email: string) {
-  const result = await pool.query(queries.users.findUserByEmail, [email]);
-  return result;
-}
-async function getUserByAlias(alias: string) {
-  const result = await pool.query(queries.users.findUserByAlias, [alias]);
+  const result = await pool.query<User>(queries.users.findUserByEmail, [email]);
   return result;
 }
 
-export { getUserByEmail, getUserByAlias };
+async function getUserByAlias(alias: string) {
+  const result = await pool.query<User>(queries.users.findUserByAlias, [alias]);
+  return result;
+}
+
+async function updateUser(user: AtLeast<User, 'id'>) {
+  const result = await pool.query<User>(queries.users.updateUser, [
+    user.firstName,
+    user.lastName,
+    user.alias,
+    user.email,
+    user.password,
+    user.refreshToken,
+    user.balance,
+    user.id,
+  ]);
+  return result;
+}
+
+export { getUserByEmail, getUserByAlias, updateUser };
